e58106a263461344fba6c3bc0b2aaa0396746a3e
Het Bhavsar <hetjbhavsar@gmail.com>
2022-07-20 19:45:41 +0530

    updated contract and verified one

--- contracts/interlynk.sol
+++ contracts/interlynk.sol
@@ -1,14 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import  "@openzeppelin/contracts/access/Ownable.sol";
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+contract interlynk is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, OwnableUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
 
-
-contract interlynk is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("interlynk", "ILK") {
-        _mint(msg.sender, 10000000 );
+    function initialize() initializer public {
+        __ERC20_init("Interlynk", "ILK");
+        __ERC20Burnable_init();
+        __Ownable_init();
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
